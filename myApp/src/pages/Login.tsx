ad31ea792ebacddea48ae05c81abe1988a7a95ca
FinnHagan <2410686@dundee.ac.uk>
2024-02-17 18:48:24 +0000

    Set up login and register page
    - Can go in between login and register page
    - Register page set up
    - Fixed baxter font style so all styles can be used (regular is set to default and can specify elements you want to change)
    -

--- myApp/src/pages/Login.tsx
+++ myApp/src/pages/Login.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { IonButton, IonCard, IonCardContent, IonContent, IonFooter, IonHeader, IonInput, IonItem, IonLabel, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonCard, IonCardContent, IonContent, IonFooter, IonHeader, IonIcon, IonInput, IonItem, IonLabel, IonPage, IonTitle, IonToolbar, useIonRouter } from '@ionic/react';
+import { arrowForwardCircleOutline, personCircleOutline } from 'ionicons/icons';
+import loginImage from '../assets/Login-screen-image.jpg';
 
 const Login: React.FC = () => {
+    const router = useIonRouter();
     const handleLogin = (event: any) => {
         event.preventDefault();
         console.log('Login');
+        // router.push('/home', 'root');
     }
     return (
         <IonPage>
@@ -14,14 +18,23 @@ const Login: React.FC = () => {
                 </IonToolbar>
             </IonHeader>
 
-            <IonContent>
+            <IonContent scrollY={false}>
                 <IonCard>
+                    <div>
+                        <img src={loginImage} alt="Login Page"></img>
+                    </div>
                     <IonCardContent>
                         <form onSubmit={handleLogin}>
                             <IonInput fill="outline" labelPlacement="floating" label="Email" type="email" placeholder="uod@dundee.ac.uk" required></IonInput>
                             <IonInput className="ion-margin-top" fill="outline" labelPlacement="floating" label="Password" type="password" required></IonInput>
-                            <IonButton className="ion-margin-top" expand="block" type="submit" shape="round">Login</IonButton>
-                            <IonButton className="ion-margin-top" routerLink="/register" expand="block" shape="round" color="tertiary" >Create Account</IonButton>
+                            <IonButton className="ion-margin-top font-bold" expand="block" type="submit" shape="round">
+                                Login
+                                <IonIcon slot="end" icon={arrowForwardCircleOutline} />
+                            </IonButton>
+                            <IonButton className="ion-margin-top font-bold" routerLink="/register" expand="block" shape="round" color="success" >
+                                Create Account
+                                <IonIcon slot="end" icon={personCircleOutline} />
+                            </IonButton>
                         </form>
                     </IonCardContent>
                 </IonCard>
