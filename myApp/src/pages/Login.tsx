c27b595ff2374fc2c60d210b716f61ac78c0e89a
FinnHagan <2410686@dundee.ac.uk>
2024-02-18 23:33:03 +0000

    Set-up basic intro page
    - Created 4 intro swipe slides (will probably change but just there for now)
    
    - ToDo: need to include logic for saving the information so they don't have to keep going through the introduction every time

--- myApp/src/pages/Login.tsx
+++ myApp/src/pages/Login.tsx
@@ -1,45 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IonButton, IonCard, IonCardContent, IonContent, IonFooter, IonHeader, IonIcon, IonInput, IonItem, IonLabel, IonPage, IonTitle, IonToolbar, useIonRouter } from '@ionic/react';
 import { arrowForwardCircleOutline, personCircleOutline } from 'ionicons/icons';
 import loginImage from '../assets/Login-screen-image.jpg';
+import Introduction from '../components/Introduction';
 
 const Login: React.FC = () => {
     const router = useIonRouter();
+    const [introViewed, setIntroViewed] = useState(false);
+
     const handleLogin = (event: any) => {
         event.preventDefault();
         console.log('Login');
-        // router.push('/home', 'root');
-    }
-    return (
-        <IonPage>
-            <IonHeader>
-                <IonToolbar color='primary'>
-                    <IonTitle>Should I Put My Washing On?</IonTitle>
-                </IonToolbar>
-            </IonHeader>
 
-            <IonContent>
-                <IonCard>
-                    <IonCardContent>
-                        <div>
-                            <img src={loginImage} alt="Login Page"></img>
-                        </div>
-                        <form onSubmit={handleLogin}>
-                            <IonInput fill="outline" labelPlacement="floating" label="Email" type="email" placeholder="uod@dundee.ac.uk" required></IonInput>
-                            <IonInput className="ion-margin-top" fill="outline" labelPlacement="floating" label="Password" type="password" required></IonInput>
-                            <IonButton className="ion-margin-top font-bold" expand="block" type="submit" shape="round">
-                                Login
-                                <IonIcon slot="end" icon={arrowForwardCircleOutline} />
-                            </IonButton>
-                            <IonButton className="ion-margin-top font-bold" routerLink="/register" expand="block" shape="round" color="success" >
-                                Create Account
-                                <IonIcon slot="end" icon={personCircleOutline} />
-                            </IonButton>
-                        </form>
-                    </IonCardContent>
-                </IonCard>
-            </IonContent>
-        </IonPage>
+        // router.push('/home', 'root');
+    };
+
+    const introDone = () => {
+        console.log('Intro viewed');
+        setIntroViewed(true);
+    };
+
+    return (
+        <>
+            {!introViewed ? (
+                <Introduction onIntroViewed={introDone} />
+            ) : (
+                <IonPage>
+                    <IonHeader>
+                        <IonToolbar color='primary'>
+                            <IonTitle>Should I Put My Washing On?</IonTitle>
+                        </IonToolbar>
+                    </IonHeader>
+
+                    <IonContent>
+                        <IonCard>
+                            <IonCardContent>
+                                <div>
+                                    <img src={loginImage} alt="Login Page"></img>
+                                </div>
+                                <form onSubmit={handleLogin}>
+                                    <IonInput fill="outline" labelPlacement="floating" label="Email" type="email" placeholder="uod@dundee.ac.uk" required></IonInput>
+                                    <IonInput className="ion-margin-top" fill="outline" labelPlacement="floating" label="Password" type="password" required></IonInput>
+                                    <IonButton className="ion-margin-top font-bold" expand="block" type="submit" shape="round">
+                                        Login
+                                        <IonIcon slot="end" icon={arrowForwardCircleOutline} />
+                                    </IonButton>
+                                    <IonButton className="ion-margin-top font-bold" routerLink="/register" expand="block" shape="round" color="success" >
+                                        Create Account
+                                        <IonIcon slot="end" icon={personCircleOutline} />
+                                    </IonButton>
+                                </form>
+                            </IonCardContent>
+                        </IonCard>
+                    </IonContent>
+                </IonPage>
+            )}
+        </>
     );
 };
 
