e397cdf9c6e7a022c8839b975ee91642c8250964
FinnHagan <2410686@dundee.ac.uk>
2024-02-20 18:53:57 +0000

    Set-up basic landing page (not finished, doesn't have all required fields)

--- myApp/src/App.tsx
+++ myApp/src/App.tsx
@@ -22,6 +22,7 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 import Register from './pages/Register';
+import LandingPage from './pages/LandingPage';
 
 setupIonicReact();
 
@@ -33,6 +34,7 @@ const App: React.FC = () => (
           <Login />
         </Route>
         <Route component={Register} path="/register" exact />
+        <Route component={LandingPage} path="/app" exact />
       </IonRouterOutlet>
     </IonReactRouter>
   </IonApp>
