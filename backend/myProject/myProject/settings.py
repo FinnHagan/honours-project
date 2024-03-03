e5f1e70056299ffb46ed78cd4db30f919487fbe1
FinnHagan <2410686@dundee.ac.uk>
2024-03-03 12:59:23 +0000

    Updated allowed hosts to include frontend url
    Also added frontend github actions

--- backend/myProject/myProject/settings.py
+++ backend/myProject/myProject/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "https://honours-django-backend-gvtynwpioq-uc.a.run.app",
+    "https://api.finnhagan.co.uk",
+    "http://localhost:8000",
+]
 
 
 # Application definition
