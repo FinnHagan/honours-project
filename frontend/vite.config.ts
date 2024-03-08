c9d7e42abff4a1a72807d5c9fa2809459fd34a70
FinnHagan <2410686@dundee.ac.uk>
2024-03-08 11:51:23 +0000

    Updated vite.config to see if it processes URL
    trying with two separate prefixes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,28 @@
-import legacy from '@vitejs/plugin-legacy'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import legacy from '@vitejs/plugin-legacy';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
-// https://vitejs.dev/config/
+const envFiles = {
+  development: '.env.development',
+  mobile: '.env.local',
+};
+dotenv.config({ path: envFiles[process.env.MODE] });
+
 export default defineConfig({
   plugins: [
     react(),
     legacy()
   ],
-})
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "~": fileURLToPath(new URL("./frontend", import.meta.url))
+    }
+  },
+  define: {
+    'process.env': process.env
+  }
+});
