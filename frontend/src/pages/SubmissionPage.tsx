51e1449c115d2884ef5c7b3778bd35e1ad34e9b6
FinnHagan <2410686@dundee.ac.uk>
2024-03-08 13:01:55 +0000

    Update SubmissionPage.tsx

--- frontend/src/pages/SubmissionPage.tsx
+++ frontend/src/pages/SubmissionPage.tsx
@@ -10,6 +10,8 @@ const SubmissionPage: React.FC = () => {
 
     const postCodeRegex = /([A-Z]{1,2}[0-9]{1,2})([A-Z]{1,2})?(\W)?([0-9]{1,2}[A-Z]{2})?/i; // A simple regex to match UK post codes
 
+    const baseUrl = import.meta.env.BASE_URL;
+
     const handleSubmit = (event: any) => {
         event.preventDefault();
 
@@ -18,6 +20,8 @@ const SubmissionPage: React.FC = () => {
             number_of_solar_panels: solar_panels,
             date: date
         };
+
+        console.log('url:', baseUrl);
 
         axios.post(`https://api.finnhagan.co.uk/api/submission/`, data)
             .then(response => {
