bb8958112fc4f978bb5e67b250340d8125acbe08
FinnHagan <2410686@dundee.ac.uk>
2024-03-18 15:01:21 +0000

    Refactored code, got solar irridiance estimated, updated submission form to get more data

--- backend/myProject/solarApp/models.py
+++ backend/myProject/solarApp/models.py
@@ -6,6 +6,8 @@ class Submission(models.Model):
     post_code = models.CharField(max_length=10)
     number_of_solar_panels = models.IntegerField(default=1)
     date = models.DateTimeField(default=timezone.now)
+    panel_orientation = models.FloatField(max_length=50, null=True)
+    panel_tilt = models.FloatField(max_length=50, null=True)
     temperature = models.FloatField(max_length=50, null=True)
     cloud_cover = models.CharField(max_length=50, null=True)
     wind_speed = models.FloatField(max_length=50, null=True)
@@ -14,3 +16,4 @@ class Submission(models.Model):
     precipitation = models.CharField(max_length=50, null=True)
     solar_azimuth = models.FloatField(max_length=50, null=True)
     solar_altitude = models.FloatField(max_length=50, null=True)
+    solar_irradiance = models.FloatField(max_length=50, null=True)
