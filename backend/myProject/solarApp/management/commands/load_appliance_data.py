983fe9f3e9cb0f4a7818fc44ab8f1fb06780956b
FinnHagan <2410686@dundee.ac.uk>
2024-04-04 19:40:12 +0100

    seeing if making a new build fixes being able to load new appliance data

--- backend/myProject/solarApp/management/commands/load_appliance_data.py
+++ backend/myProject/solarApp/management/commands/load_appliance_data.py
@@ -1,6 +1,5 @@
 from django.core.management.base import BaseCommand
 from solarApp.models import ApplianceConsumption
-from datetime import time
 
 
 class Command(BaseCommand):
@@ -8,38 +7,38 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         predefined_data = [
-            {"appliance_name": "washing_machine", "timestamp": "08:52", "consumption": 0.37},
-            {"appliance_name": "washing_machine", "timestamp": "09:12", "consumption": 375.23},
-            {"appliance_name": "washing_machine", "timestamp": "09:22", "consumption": 375.68},
-            {"appliance_name": "washing_machine", "timestamp": "09:32", "consumption": 140.23},
-            {"appliance_name": "washing_machine", "timestamp": "09:42", "consumption": 62.92},
-            {"appliance_name": "washing_machine", "timestamp": "09:52", "consumption": 58.33},
-            {"appliance_name": "washing_machine", "timestamp": "10:02", "consumption": 11.57},
-            {"appliance_name": "washing_machine", "timestamp": "10:12", "consumption": 10.9},
-            {"appliance_name": "washing_machine", "timestamp": "10:22", "consumption": 11},
-            {"appliance_name": "washing_machine", "timestamp": "10:32", "consumption": 18.38},
-            {"appliance_name": "washing_machine", "timestamp": "10:42", "consumption": 18.77},
-            {"appliance_name": "washing_machine", "timestamp": "10:52", "consumption": 0.32},
-            {"appliance_name": "tumble_dryer", "timestamp": "12:57", "consumption": 0.03},
-            {"appliance_name": "tumble_dryer", "timestamp": "13:07", "consumption": 298.37},
-            {"appliance_name": "tumble_dryer", "timestamp": "13:17", "consumption": 312.7},
-            {"appliance_name": "tumble_dryer", "timestamp": "13:27", "consumption": 279.18},
-            {"appliance_name": "tumble_dryer", "timestamp": "13:37", "consumption": 278.23},
-            {"appliance_name": "tumble_dryer", "timestamp": "13:47", "consumption": 275.85},
-            {"appliance_name": "tumble_dryer", "timestamp": "13:57", "consumption": 251.97},
-            {"appliance_name": "tumble_dryer", "timestamp": "14:07", "consumption": 34.95},
-            {"appliance_name": "tumble_dryer", "timestamp": "14:17", "consumption": 187.23},
-            {"appliance_name": "tumble_dryer", "timestamp": "14:27", "consumption": 281.2},
-            {"appliance_name": "tumble_dryer", "timestamp": "14:37", "consumption": 185.17},
-            {"appliance_name": "tumble_dryer", "timestamp": "14:47", "consumption": 16.82},
-            {"appliance_name": "tumble_dryer", "timestamp": "14:57", "consumption": 0.02},
+            {"appliance_name": "washing_machine", "sequence": 1, "consumption": 0.37},
+            {"appliance_name": "washing_machine", "sequence": 2, "consumption": 375.23},
+            {"appliance_name": "washing_machine", "sequence": 3, "consumption": 375.68},
+            {"appliance_name": "washing_machine", "sequence": 4, "consumption": 140.23},
+            {"appliance_name": "washing_machine", "sequence": 5, "consumption": 62.92},
+            {"appliance_name": "washing_machine", "sequence": 6, "consumption": 58.33},
+            {"appliance_name": "washing_machine", "sequence": 7, "consumption": 11.57},
+            {"appliance_name": "washing_machine", "sequence": 8, "consumption": 10.9},
+            {"appliance_name": "washing_machine", "sequence": 9, "consumption": 11},
+            {"appliance_name": "washing_machine", "sequence": 10, "consumption": 18.38},
+            {"appliance_name": "washing_machine", "sequence": 11, "consumption": 18.77},
+            {"appliance_name": "washing_machine", "sequence": 12, "consumption": 0.32},
+            {"appliance_name": "washing_machine", "sequence": 13, "consumption": 0.05},
+            {"appliance_name": "tumble_dryer", "sequence": 1, "consumption": 0.03},
+            {"appliance_name": "tumble_dryer", "sequence": 2, "consumption": 298.37},
+            {"appliance_name": "tumble_dryer", "sequence": 3, "consumption": 312.7},
+            {"appliance_name": "tumble_dryer", "sequence": 4, "consumption": 279.18},
+            {"appliance_name": "tumble_dryer", "sequence": 5, "consumption": 278.23},
+            {"appliance_name": "tumble_dryer", "sequence": 6, "consumption": 275.85},
+            {"appliance_name": "tumble_dryer", "sequence": 7, "consumption": 251.97},
+            {"appliance_name": "tumble_dryer", "sequence": 8, "consumption": 34.95},
+            {"appliance_name": "tumble_dryer", "sequence": 9, "consumption": 187.23},
+            {"appliance_name": "tumble_dryer", "sequence": 10, "consumption": 281.2},
+            {"appliance_name": "tumble_dryer", "sequence": 11, "consumption": 185.17},
+            {"appliance_name": "tumble_dryer", "sequence": 12, "consumption": 16.82},
+            {"appliance_name": "tumble_dryer", "sequence": 13, "consumption": 0.02},
         ]
 
         for record in predefined_data:
-            record['timestamp'] = time.fromisoformat(record['timestamp'])
             obj, created = ApplianceConsumption.objects.get_or_create(**record)
             if created:
-                self.stdout.write(self.style.SUCCESS(f'Added new record for {record["appliance_name"]} at {record["timestamp"]}'))
+                self.stdout.write(self.style.SUCCESS(f'Added new record for {record["appliance_name"]} at sequence {record["sequence"]}'))
             else:
-                self.stdout.write(self.style.WARNING(f'Record already exists for {record["appliance_name"]} at {record["timestamp"]}'))
-                self.stdout.write(self.style.SUCCESS('Successfully loaded appliance consumption data'))
+                self.stdout.write(self.style.WARNING(f'Record already exists for {record["appliance_name"]} at sequence {record["sequence"]}'))
+        self.stdout.write(self.style.SUCCESS('Successfully loaded appliance consumption data'))
